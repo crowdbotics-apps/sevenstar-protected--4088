5d92d7acec263aebde145796530ac3b752df12f0
SHABEEB-LAPTOP\shabe <shabeebhasan@hotmail.com>
2019-07-24 00:21:53 +0500

    UIs work completed

--- frontend/src/containers/ChooseRole/index.js
+++ frontend/src/containers/ChooseRole/index.js
@@ -1,105 +1,150 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import {Image, TouchableOpacity, View} from 'react-native';
 import {
-  Image,
-  TouchableOpacity,
-  View,
-} from 'react-native';
-import {
-  Button,
-  Container,
-  Content,
-  Form,
-  Item,
-  Input,
-  Text,
+    Button,
+    Container,
+    Content,
+    Form,
+    Item,
+    Input,
+    Text
 } from 'native-base';
+import {Ionicons} from '@expo/vector-icons';
 
 import styles from './styles';
 
 class ChooseRole extends Component {
-  state = {
-    selected_role: '',
-  };
+    state = {
+        selected_role: ''
+    };
 
-  // navigate to home after request of forgot password
-  onProceedButtonPressed = () => {
-    // TODO: Forgot Password
-    if(this.state.selected_role == 'officer'){
-      this.props.navigation.navigate('OfficerSignUp');
+    // navigate to home after request of forgot password
+    onProceedButtonPressed = () => {
+        // TODO: Forgot Password
+        if (this.state.selected_role == 'officer') {
+            this
+                .props
+                .navigation
+                .navigate('OfficerSignUp');
+        }
+        if (this.state.selected_role == 'citizen') {
+            this
+                .props
+                .navigation
+                .navigate('CitizenSignUp');
+        }
+        return;
+        this
+            .props
+            .navigation
+            .navigate('Signup');
     }
-    if(this.state.selected_role == 'citizen'){
-      this.props.navigation.navigate('CitizenSignUp');
+
+    // navigate to home after request of forgot password
+    onRolesButtonPressed = (role) => {
+        this.setState({selected_role: role})
     }
-    return;
-    this.props.navigation.navigate('Signup');
-  }
 
-  // navigate to home after request of forgot password
-  onRolesButtonPressed = (role) => {
-    this.setState({selected_role:role})
-  }
+    // navigate to login screen
+    onLoginButtonPressed = () => {
+        this
+            .props
+            .navigation
+            .navigate('Login');
+    }
 
-  // navigate to login screen
-  onLoginButtonPressed = () => {
-    this.props.navigation.navigate('Login');
-  }
+    render() {
+        return (
+            <Container style={styles.container}>
+                <Content contentContainerStyle={styles.content}>
 
-  render() {
-    return (
-      <Container style={styles.container}>
-        <Content contentContainerStyle={styles.content}>
-          {/* Logo */}
-          <View style={styles.logoContainer}>
-            <Image
-              style={styles.logo}
-              source={require('../../assets/images/logo.png')}
-            />
-            <Text style={styles.logoText}>Choose Your Role</Text>
-          </View>
+                    <TouchableOpacity
+                        onPress={() => {
+                        this
+                            .props
+                            .navigation
+                            .navigate('Login');
+                    }}
+                        style={{
+                        width: 50,
+                        height: 30,
+                        flexDirection: 'row',
+                        alignItems: 'center'
+                    }}>
+                        <Ionicons name="ios-arrow-back" size={25} color="#333"/>
+                        <Text
+                            style={{
+                            color: '#333',
+                            marginStart: 5
+                        }}>Back</Text>
+                    </TouchableOpacity>
 
-          {/* Form */}
-          <Form style={styles.form}>
-            
-            <Button
-              style={[styles.button,{borderColor:'transparent',height:80, backgroundColor:this.state.selected_role == 'officer' ? "#2BA4DD80":"#2BA4DD"}]}
-              onPress={()=>{this.onRolesButtonPressed("officer")}}
-              hasText
-              block
-              large
-            >
-              <Text style={styles.sendText}>I'm the Officer</Text>
-            </Button>
+                    {/* Logo */}
+                    <View style={styles.logoContainer}>
+                        <Image style={styles.logo} source={require('../../assets/images/logo.png')}/>
+                        <Text style={styles.logoText}>Choose Your Role</Text>
+                    </View>
 
-            <Button
-              style={[styles.button,{borderColor:'transparent',height:80,backgroundColor:this.state.selected_role == 'citizen' ? "#2BA4DD80":"#2BA4DD"}]}
-              onPress={()=>{this.onRolesButtonPressed("citizen")}}
-              hasText
-              block
-              large
-            >
-              <Text style={styles.sendText}>I'm the Citizen</Text>
-            </Button>
+                    {/* Form */}
+                    <Form style={styles.form}>
 
-          </Form>
+                        <Button
+                            style={[
+                            styles.button, {
+                                borderColor: 'transparent',
+                                height: 80,
+                                backgroundColor: this.state.selected_role == 'officer'
+                                    ? "#2BA4DD80"
+                                    : "#2BA4DD"
+                            }
+                        ]}
+                            onPress={() => {
+                            this.onRolesButtonPressed("officer")
+                        }}
+                            hasText
+                            block
+                            large>
+                            <Text style={styles.sendText}>I'm the Officer</Text>
+                        </Button>
 
-          <View style={styles.buttonContainer}>
-            {/* Login Button */}
-            <Button
-              style={styles.button}
-              onPress={this.onProceedButtonPressed}
-              hasText
-              block
-              large
-              dark
-            >
-              <Text style={styles.sendText}>PROCEED</Text>
-            </Button>
+                        <Button
+                            style={[
+                            styles.button, {
+                                borderColor: 'transparent',
+                                height: 80,
+                                backgroundColor: this.state.selected_role == 'citizen'
+                                    ? "#2BA4DD80"
+                                    : "#2BA4DD"
+                            }
+                        ]}
+                            onPress={() => {
+                            this.onRolesButtonPressed("citizen")
+                        }}
+                            hasText
+                            block
+                            large>
+                            <Text style={styles.sendText}>I'm the Citizen</Text>
+                        </Button>
 
-          </View>
-        </Content>
-      </Container>
-    );
-  }
+                    </Form>
+
+                    <View style={styles.buttonContainer}>
+                        {/* Login Button */}
+                        <Button
+                            style={styles.button}
+                            onPress={this.onProceedButtonPressed}
+                            hasText
+                            block
+                            large
+                            dark>
+                            <Text style={styles.sendText}>PROCEED</Text>
+                        </Button>
+
+                    </View>
+                </Content>
+            </Container>
+        );
+    }
 }
 
 export default ChooseRole;
